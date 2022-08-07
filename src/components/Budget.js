292d0d53814c085c597949665c3c7fa56be1ef79
hala277 <hala.almasharfeh@gmail.com>
2022-08-07 13:36:28 +0300

    add new thing

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import ViewBudget from "./ViewBudget";
+import EditBudget from "./EditBudget";
 
 const Budget = () => {
   // use destructuring to get the budget from context
-  const { budget } = useContext(AppContext);
+  const { budget, dispatch } = useContext(AppContext);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = (value) => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: value,
+    });
+    setIsEditing(false);
+  };
   return (
     // Bootstrap Alert classes to gives us a gray background
-    <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+    <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
+      {isEditing ? (
+        <EditBudget handleSaveClick={handleSaveClick} budget={budget} />
+      ) : (
+        <ViewBudget handleEditClick={handleEditClick} budget={budget} />
+      )}
     </div>
   );
 };
